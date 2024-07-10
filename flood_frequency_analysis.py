d62f50667ada7b7baf8d0a041d71d0aef5982212
claire-middendorf <77594367+claire-middendorf@users.noreply.github.com>
2024-07-10 16:46:42 -0500

    Update flood_frequency_analysis.py

--- flood_frequency_analysis.py
+++ flood_frequency_analysis.py
@@ -166,39 +166,42 @@ else:
         data, floods = gumbel_stats(peakflow)
         
         # step 6 - visualization
-        unit_hydro = pd.read_csv(HYDRO)
-        interval = T_INT
-    
+        unit_hydro = pd.read_csv('/Users/clairemiddendorf/Desktop/USACE Internship/flood_python/hydrograph.csv') 
+        interval = 2
+            
         hydrograph = pd.DataFrame()
-    
+            
         hydrograph['t'] = unit_hydro['t/tp'].apply(lambda x: (x * interval)) 
-        count = 0 
-        for i in floods: 
-            hydrograph['{floods.loc[count,1]}-year Flood'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * floods.loc[count,2))
-            count = count+1
         
+        count = 0 
+        length = len(floods.index)
+        while count < length:
+            hydrograph[str(floods.loc[count,'Return Periods']) +'-year Flood'] = unit_hydro['q/qp'].apply(
+            lambda x: (x * floods.loc[count,'Peak Streamflow (cfs)']))
+            count = count+1
+                
         # Create a line chart
         fig, ax = plt.subplots()
-
-        count = 0
-        for i in hydrograph:   
-            plt.plot(hydrograph['t'], hydrograph.loc[:,count), linestyle='-', label="1-year")
+        
+        count = 1
+        length2 = len(hydrograph.columns)
+        while count < length:   
+            plt.plot(hydrograph['t'], hydrograph.iloc[:,count], linestyle='-', label=str(floods.loc[count,'Return Periods']) +'-year Flood')
             count = count+1
+                    
             
-    
         # Add title and labels
         plt.title('Hydrograph')
         plt.xlabel('Time (hours)')
         plt.ylabel('Flow Rate (cfs)')
         ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
-    
+            
         # Display grid
         plt.grid(True)
-    
+            
         plt.legend(loc='best')
         plt.tight_layout()
-    
+            
         # Show the plot
         plt.show()
 
