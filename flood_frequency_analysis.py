bfe4e879d824617d1cb3b181555c9f231f1b7903
claire-middendorf <77594367+claire-middendorf@users.noreply.github.com>
2024-07-11 16:15:03 -0500

    Update flood_frequency_analysis.py

--- flood_frequency_analysis.py
+++ flood_frequency_analysis.py
@@ -12,20 +12,29 @@ Claire Middendorf
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
-import log_pearson_analysis as lp
+import log_pearson_methods as lp
 import k_table as kdata
-import gumbel_analysis as gb
+import gumbel_methods as gb
+from IPython.display import display
+import seaborn as sns
+import random
 
 # globals
-INPUT = './src/jc_station.csv'
-REGIONAL_SKEW = -0.3
-T_INT = 2
+INPUT = 'station_rulo.csv' #csv file with peak streamflow data
+REGIONAL_SKEW = -0.3 #regional skew for river station (use map image) for Log-Pearson
+flood_duration = 58 #desired duration of flood (days)
+base_flow = 50714 #base flow of river
+HYDRO = 'hydrograph.csv' #unit hydrograph csv file
+return_periods = [1.01,2,5,10,25,50,100] #desired return periods for Gumbel Method
 
-method = input("Which flood frequency analysis? Write 1 for Log-Pearson Type III or 2 for Gumbel: ")
+#flood frequency analysis: Write 1 for Log-Pearson Type III or 2 for Gumbel
+method = 1
+
 
 def main():
-
-if method = 1:
+    
+    #log-pearson type III method
+    if method == 1:
         # step 1 - read in peakflow data
         peak_flow_df = pd.read_csv(INPUT,
                                    header=None,
@@ -95,47 +104,55 @@ if method = 1:
             "Two Hundred-Year Flood": [two_hundred]
         }
         ff_results = pd.DataFrame(flood_frequencies)
-    
-        print(ff_results)
+        
+        pd.set_option('display.max_columns', None)
+        display(ff_results)
 
         # step 6 - visualization
         unit_hydro = pd.read_csv(HYDRO)
-        interval = T_INT
+        
     
         hydrograph = pd.DataFrame()
-    
+        
+        interval = flood_duration/5
         hydrograph['t'] = unit_hydro['t/tp'].apply(lambda x: (x * interval))
+        
+        
         hydrograph['1-year'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * ff_results['One-Year Flood']))
+            lambda x: (x * ff_results['One-Year Flood']) + base_flow)
         hydrograph['2-year'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * ff_results['Two-Year Flood']))
+            lambda x: (x * ff_results['Two-Year Flood'])+ base_flow)
         hydrograph['5-year'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * ff_results['Five-Year Flood']))
+            lambda x: (x * ff_results['Five-Year Flood'])+ base_flow)
         hydrograph['10-year'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * ff_results['Ten-Year Flood']))
+            lambda x: (x * ff_results['Ten-Year Flood'])+ base_flow)
         hydrograph['25-year'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * ff_results['Twenty-five-Year Flood']))
+            lambda x: (x * ff_results['Twenty-five-Year Flood'])+ base_flow)
         hydrograph['50-year'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * ff_results['Fifty-Year Flood']))
+            lambda x: (x * ff_results['Fifty-Year Flood'])+ base_flow)
         hydrograph['100-year'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * ff_results['Hundred-Year Flood']))
+            lambda x: (x * ff_results['Hundred-Year Flood'])+ base_flow)
         hydrograph['200-year'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * ff_results['Two Hundred-Year Flood']))
+            lambda x: (x * ff_results['Two Hundred-Year Flood'])+ base_flow)
     
+        display(hydrograph)
+        
+        
         # Create a line chart
         fig, ax = plt.subplots()
-        plt.plot(hydrograph['t'], hydrograph['1-year'], linestyle='-', label="1-year")
-        plt.plot(hydrograph['t'], hydrograph['2-year'], linestyle='-', label="2-year")
-        plt.plot(hydrograph['t'], hydrograph['5-year'], linestyle='-', label="5-year")
-        plt.plot(hydrograph['t'], hydrograph['10-year'], linestyle='-', label="10-year")
-        plt.plot(hydrograph['t'], hydrograph['25-year'], linestyle='-', label="25-year")
-        plt.plot(hydrograph['t'], hydrograph['50-year'], linestyle='-', label="50-year")
-        plt.plot(hydrograph['t'], hydrograph['100-year'], linestyle='-', label="100-year")
-        plt.plot(hydrograph['t'], hydrograph['200-year'], linestyle='-', label="200-year")
+        palette=sns.color_palette(palette='Blues_d')
+        plt.plot(hydrograph['t'], hydrograph['1-year'],color = random.choice(palette), linestyle='-', label="1-year")
+        plt.plot(hydrograph['t'], hydrograph['2-year'], color = random.choice(palette), linestyle='-', label="2-year")
+        plt.plot(hydrograph['t'], hydrograph['5-year'], color = random.choice(palette), linestyle='-', label="5-year")
+        plt.plot(hydrograph['t'], hydrograph['10-year'], color = random.choice(palette), linestyle='-', label="10-year")
+        plt.plot(hydrograph['t'], hydrograph['25-year'], color = random.choice(palette), linestyle='-', label="25-year")
+        plt.plot(hydrograph['t'], hydrograph['50-year'], color = random.choice(palette), linestyle='-', label="50-year")
+        plt.plot(hydrograph['t'], hydrograph['100-year'], color = random.choice(palette), linestyle='-', label="100-year")
+        plt.plot(hydrograph['t'], hydrograph['200-year'], color = random.choice(palette), linestyle='-', label="200-year")
     
         # Add title and labels
         plt.title('Hydrograph')
-        plt.xlabel('Time (hours)')
+        plt.xlabel('Time (days)')
         plt.ylabel('Flow Rate (cfs)')
         ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
     
@@ -147,49 +164,58 @@ if method = 1:
     
         # Show the plot
         plt.show()
-
-#Gumbel Analysis
-else: 
+    
+    #Gumbel Analysis
+    else: 
 
         peak_flow_df = pd.read_csv(INPUT,
                                        header=None,
                                        names=['date', 'peak_streamflow'])
         
-        return_periods = input("List the return periods for the desired peak streamflows: ")
-        return_periods = return_periods.split(',')
-        count = 0
-        for i in return_periods:
-            return_periods[count] = float(i)
-            count = count + 1
             
-        peakflow = process_peakflow_data(peak_flow_df)
+        peakflow = gb.process_peakflow_data(peak_flow_df)
         
-        data, floods = gumbel_stats(peakflow,return_periods)
+        data, floods = gb.gumbel_stats(peakflow,return_periods)
+        
+        pd.set_option('display.max_columns', None)
+        display(floods)
         
         # step 6 - visualization
+        unit_hydro = pd.read_csv(HYDRO)
+        
+    
+        hydrograph = pd.DataFrame()
+        
+        interval = flood_duration/5
+        hydrograph['t'] = unit_hydro['t/tp'].apply(lambda x: (x * interval))
+        
         count = 0 
         length = len(floods.index)
         while count < length:
-            hydrograph[str(floods.iloc[count,1]) +'-year Flood'] = unit_hydro['q/qp'].apply(
-            lambda x: (x * floods.iloc[count,2]))
+            hydrograph[str(floods.iloc[count,0]) +'-year Flood'] = unit_hydro['q/qp'].apply(
+            lambda x: (x * floods.iloc[count,1])+ base_flow)
             count = count+1
-                
+        
+        display(hydrograph)
+        
         # Create a line chart
         fig, ax = plt.subplots()
         
+        palette=sns.color_palette(palette='Blues_d')
+        
         count = 1
-        length2 = len(hydrograph.columns)
         while count < length:   
-            plt.plot(hydrograph['t'], hydrograph.iloc[:,count], linestyle='-', label=str(floods.iloc[count,1]) +'-year Flood')
+            plt.plot(hydrograph['t'], hydrograph.iloc[:,count], color = random.choice(palette), label=str(floods.iloc[count,0]) +'-year Flood')
             count = count+1
                     
             
         # Add title and labels
         plt.title('Hydrograph')
-        plt.xlabel('Time (hours)')
+        plt.xlabel('Time (Days)')
         plt.ylabel('Flow Rate (cfs)')
         ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
-            
+        
+        
         # Display grid
         plt.grid(True)
             
@@ -200,3 +226,7 @@ else:
         plt.show()
 
 
+if __name__ == '__main__':
+    main()
+    
+    
