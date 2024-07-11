0d9eca5de063f49e89d2250a350b7c21afd1ad64
claire-middendorf <77594367+claire-middendorf@users.noreply.github.com>
2024-07-10 20:50:13 -0500

    Update hydrograph.py

--- hydrograph.py
+++ hydrograph.py
@@ -1,54 +1,47 @@
 '''
-Creates a hydrograph of user's specified duration
+Given a file with return periods and peak streamflows, this code creates a hydrograph of user's specified duration
 
 '''
 
+HYDRO = 'hydrograph.csv'
+INT = 2
+floods = 'floods.csv' #a file with return periods and peak streamflows
 
-    unit_hydro = pd.read_csv(HYDRO)
-    interval = T_INT
-
-    hydrograph = pd.DataFrame()
-
-    hydrograph['t'] = unit_hydro['t/tp'].apply(lambda x: (x * interval))
-    hydrograph['1-year'] = unit_hydro['q/qp'].apply(
-        lambda x: (x * ff_results['One-Year Flood']))
-    hydrograph['2-year'] = unit_hydro['q/qp'].apply(
-        lambda x: (x * ff_results['Two-Year Flood']))
-    hydrograph['5-year'] = unit_hydro['q/qp'].apply(
-        lambda x: (x * ff_results['Five-Year Flood']))
-    hydrograph['10-year'] = unit_hydro['q/qp'].apply(
-        lambda x: (x * ff_results['Ten-Year Flood']))
-    hydrograph['25-year'] = unit_hydro['q/qp'].apply(
-        lambda x: (x * ff_results['Twenty-five-Year Flood']))
-    hydrograph['50-year'] = unit_hydro['q/qp'].apply(
-        lambda x: (x * ff_results['Fifty-Year Flood']))
-    hydrograph['100-year'] = unit_hydro['q/qp'].apply(
-        lambda x: (x * ff_results['Hundred-Year Flood']))
-    hydrograph['200-year'] = unit_hydro['q/qp'].apply(
-        lambda x: (x * ff_results['Two Hundred-Year Flood']))
-
-    # Create a line chart
-    fig, ax = plt.subplots()
-    plt.plot(hydrograph['t'], hydrograph['1-year'], linestyle='-', label="1-year")
-    plt.plot(hydrograph['t'], hydrograph['2-year'], linestyle='-', label="2-year")
-    plt.plot(hydrograph['t'], hydrograph['5-year'], linestyle='-', label="5-year")
-    plt.plot(hydrograph['t'], hydrograph['10-year'], linestyle='-', label="10-year")
-    plt.plot(hydrograph['t'], hydrograph['25-year'], linestyle='-', label="25-year")
-    plt.plot(hydrograph['t'], hydrograph['50-year'], linestyle='-', label="50-year")
-    plt.plot(hydrograph['t'], hydrograph['100-year'], linestyle='-', label="100-year")
-    plt.plot(hydrograph['t'], hydrograph['200-year'], linestyle='-', label="200-year")
-
-    # Add title and labels
-    plt.title('Hydrograph')
-    plt.xlabel('Time (hours)')
-    plt.ylabel('Flow Rate (cfs)')
-    ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
-
-    # Display grid
-    plt.grid(True)
-
-    plt.legend(loc='best')
-    plt.tight_layout()
-
-    # Show the plot
-    plt.show()
+        unit_hydro = pd.read_csv(HYDRO) 
+        interval = INT
+            
+        hydrograph = pd.DataFrame()
+            
+        hydrograph['t'] = unit_hydro['t/tp'].apply(lambda x: (x * interval)) 
+        
+        count = 0 
+        length = len(floods.index)
+        while count < length:
+            hydrograph[str(floods.iloc[count,1]) +'-year Flood'] = unit_hydro['q/qp'].apply(
+            lambda x: (x * floods.iloc[count,2]))
+            count = count+1
+                
+        # Create a line chart
+        fig, ax = plt.subplots()
+        
+        count = 1
+        length2 = len(hydrograph.columns)
+        while count < length:   
+            plt.plot(hydrograph['t'], hydrograph.iloc[:,count], linestyle='-', label=str(floods.iloc[count,1]) +'-year Flood')
+            count = count+1
+                    
+            
+        # Add title and labels
+        plt.title('Hydrograph')
+        plt.xlabel('Time')
+        plt.ylabel('Flow Rate (cfs)')
+        ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+            
+        # Display grid
+        plt.grid(True)
+            
+        plt.legend(loc='best')
+        plt.tight_layout()
+            
+        # Show the plot
+        plt.show()
