098e7676e857d0d932348659c4682196d2d57b5d
claire-middendorf <77594367+claire-middendorf@users.noreply.github.com>
2024-08-12 08:07:58 -0500

    Update hydro_values.py

--- hydro_values.py
+++ hydro_values.py
@@ -3,7 +3,7 @@
 """
 Claire Middendorf
 
-    Creates a unit hydrograph for a specific flood duration in 15 minute intervals
+    Creates a unit hydrograph for a specific flood duration in intervals
 
 """
 
@@ -12,19 +12,72 @@ import pandas as pd
 import numpy as np
 
 
+
 def hydro_vals(flood_duration,interval_hr):
     
     HYDRO = '/Users/clairemiddendorf/Desktop/repos/ERDC-2024-main/hydrograph.csv' #unit hydrograph csv file
+    
+    INVERSE ='/Users/clairemiddendorf/Desktop/repos/ERDC-2024-main/reverse.csv'
 
     unit_hydro = pd.read_csv(HYDRO)
     
+    unit_inverse = pd.read_csv(INVERSE)
+    
     num_points = len(unit_hydro)
+  
+    num_intervals = flood_duration * 24 * (1/interval_hr) 
     
-    num_intervals = flood_duration * 24 * (1/interval_hr)
-    
-    inter_num = round(num_intervals/(num_points-1))
-    
+    #number of points between each int
+    inter_num = round(num_intervals/5)
+     
     new_hydrograph = pd.DataFrame()
+    
+    baseflow_vals = pd.DataFrame()
+    
+    
+    x = 0
+    while x < num_points:
+        
+        if x + 1 == 33:
+            break
+        
+        if x == 0: 
+            
+            x_point = [unit_hydro.iloc[x,0],unit_hydro.iloc[x+1,0]]
+            y_point = [unit_hydro.iloc[x,1],unit_hydro.iloc[x+1,1]]
+            
+            points = round((x_point[1] - x_point[0]) * inter_num) + 1
+            
+            x_values = np.linspace(x_point[0],x_point[1],points)
+            
+            interp_values = np.interp(x_values, x_point,y_point)
+            
+            interp_df = pd.DataFrame({"t/tp": x_values,
+                        "q/qp":interp_values})
+            
+        
+        else:
+            
+            x_point = [unit_hydro.iloc[x,0],unit_hydro.iloc[x+1,0]]
+            y_point = [unit_hydro.iloc[x,1],unit_hydro.iloc[x+1,1]]
+            
+            points = round((x_point[1] - x_point[0]) * inter_num) + 1
+            
+            x_values = np.linspace(x_point[0],x_point[1],points)
+            
+            interp_values = np.interp(x_values, x_point,y_point)
+            
+            interp_df = pd.DataFrame({"t/tp": x_values,
+                        "q/qp":interp_values})
+            
+            interp_df = interp_df.drop(0)
+        
+            
+        new_hydrograph = pd.concat([new_hydrograph, interp_df], ignore_index=True, axis = 0)
+        
+        x = x+1
+        
+    
     
     x = 0
     while x < num_points:
@@ -33,10 +86,12 @@ def hydro_vals(flood_duration,interval_hr):
             break
         
         else:
-            x_point = [unit_hydro.iloc[x,0],unit_hydro.iloc[x+1,0]]
-            y_point = [unit_hydro.iloc[x,1],unit_hydro.iloc[x+1,1]]
+            x_point = [unit_inverse.iloc[x,0],unit_inverse.iloc[x+1,0]]
+            y_point = [unit_inverse.iloc[x,1],unit_inverse.iloc[x+1,1]]
             
-            x_values = np.linspace(x_point[0],x_point[1],inter_num)
+            points = round((x_point[1] - x_point[0]) * inter_num) + 1
+            
+            x_values = np.linspace(x_point[0],x_point[1],points)
             
             interp_values = np.interp(x_values, x_point,y_point)
             
@@ -44,53 +99,11 @@ def hydro_vals(flood_duration,interval_hr):
                         "q/qp":interp_values})
         
             
-        new_hydrograph = pd.concat([new_hydrograph, interp_df], ignore_index=True, axis = 0)
+        baseflow_vals = pd.concat([baseflow_vals, interp_df], ignore_index=True, axis = 0)
         
         x = x+1
-    
-    peak = inter_num * 10
-    
-    rising_list = []
-    falling_list = []
-    
-    
-    x = 0
-    while x < len(new_hydrograph):
-        
-        if x<peak:
-            rising_list.append(new_hydrograph.iloc[x,1])
-        
-        else:
-            falling_list.append(new_hydrograph.iloc[x,1])
-        
-        x = x+1
-     
-    rising_list.reverse()
-    falling_list.reverse()
-    
-    limbs = rising_list + falling_list
- 
-    baseflow_vals = pd.DataFrame({"values": limbs})
     
     
     return new_hydrograph, baseflow_vals  
     
     
-
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
